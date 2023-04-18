8ff30ac4a2d126aa18969f991dfe232b944fe679
jakeberreth <jakeberreth@gmail.com>
2023-04-18 14:14:30 -0700

    built project

---
+++ client/build/precache-manifest.8090bac4092977a228ce384262cf259d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f31f3b3559abe46a0abc",
+    "url": "/static/js/main.bad500e4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "69632d5584d9f816937c",
+    "url": "/static/css/2.a824eb4b.chunk.css"
+  },
+  {
+    "revision": "69632d5584d9f816937c",
+    "url": "/static/js/2.686a0e53.chunk.js"
+  },
+  {
+    "revision": "1a40311b33b0abcc02c2d6aac30d238b",
+    "url": "/index.html"
+  }
+];
